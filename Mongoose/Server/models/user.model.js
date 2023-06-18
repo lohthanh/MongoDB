e9375ce85b0a93c365aaddbbfc92abb262ac0a1e
Thanh Lo <116846461+lohthanh@users.noreply.github.com>
2023-06-18 15:09:53 -0700

    Mongoose
    Implementing mongoose with mongoDB

---
+++ Mongoose/Server/models/user.model.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+ 
+const UserSchema = new mongoose.Schema({
+    name: {
+        type: String
+    },
+    age: {
+        type: Number
+    }
+}, 
+{
+    timestamps: true
+});
+ 
+const User = mongoose.model('User', UserSchema);
+ 
+
+
+module.exports = User;
